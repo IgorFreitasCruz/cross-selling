939c5d7e4f3552d51367aecbab793f17e7b5c5f4
Igor de Freitas Cruz <igor.freitas.cruz@icloud.com>
2023-11-13 17:15:25 -0300

    feat: implemented tests for client

---
+++ src/domain/client.py
@@ -0,0 +1,34 @@
+"""Module for the client entity"""
+import dataclasses
+import uuid
+from typing import Dict
+
+
+@dataclasses.dataclass
+class Client:
+    """Client entity"""
+
+    code: uuid.UUID
+    razao_social: str
+    cnpj: str
+    email: str
+
+    @classmethod
+    def from_dict(cls, d: Dict):
+        """Initialize a Client object from a dictionary
+
+        Args:
+            d (Dict): dictionary containing all Client attributes
+
+        Returns:
+            Client: Instance of Client object
+        """
+        return cls(**d)
+
+    def to_dict(self):
+        """Returns a dictinary from a Client object
+
+        Returns:
+            Dict: dictionary containg all Client attributes data
+        """
+        return dataclasses.asdict(self)
---
+++ tests/domain/test_client.py
@@ -0,0 +1,67 @@
+"""Test module for the client entity"""
+import uuid
+
+from src.domain.client import Client
+
+
+def test_client_model_init():
+    """Test client model initialization"""
+    code = uuid.uuid4()
+    client = Client(
+        code,
+        razao_social="My company",
+        cnpj="00.000.000/0000-00",
+        email="mycompany@email.com",
+    )
+
+    assert client.code == code
+    assert client.razao_social == "My company"
+    assert client.cnpj == "00.000.000/0000-00"
+    assert client.email == "mycompany@email.com"
+
+
+def test_client_model_from_dict():
+    """Test client model initialization from a dictionary"""
+    code = uuid.uuid4()
+    init_dict = {
+        "code": code,
+        "razao_social": "My company",
+        "cnpj": "00.000.000/0000-00",
+        "email": "mycompany@email.com",
+    }
+
+    client = Client.from_dict(init_dict)
+
+    assert client.code == code
+    assert client.razao_social == "My company"
+    assert client.cnpj == "00.000.000/0000-00"
+    assert client.email == "mycompany@email.com"
+
+
+def test_client_model_to_dict():
+    """Test client model conversion into a dictionary"""
+    init_dict = {
+        "code": uuid.uuid4(),
+        "razao_social": "My company",
+        "cnpj": "00.000.000/0000-00",
+        "email": "mycompany@email.com",
+    }
+
+    client = Client.from_dict(init_dict)
+
+    assert client.to_dict() == init_dict
+
+
+def test_client_model_comparison():
+    """Test client model comparison"""
+    init_dict = {
+        "code": uuid.uuid4(),
+        "razao_social": "My company",
+        "cnpj": "00.000.000/0000-00",
+        "email": "mycompany@email.com",
+    }
+
+    client1 = Client.from_dict(init_dict)
+    client2 = Client.from_dict(init_dict)
+
+    assert client1 == client2
