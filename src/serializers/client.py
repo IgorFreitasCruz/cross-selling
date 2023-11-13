04385563d7bffa180df91736cd9ecd05b13031f1
Igor de Freitas Cruz <igor.freitas.cruz@icloud.com>
2023-11-13 19:55:03 -0300

    feat: implemented client serializer

---
+++ src/serializers/client.py
@@ -0,0 +1,24 @@
+"""Module for the client serializer"""
+import json
+
+
+class ClientJsonEncoder(json.JSONEncoder):
+    """Serializer class for the client model
+
+    Args:
+        json (object): JSONEncoder
+
+    Returns:
+        str: serialized object
+    """
+    def default(self, o):
+        try:
+            to_serialize = {
+                "code": str(o.code),
+                "razao_social": o.razao_social,
+                "cnpj": o.cnpj,
+                "email": o.email,
+            }
+            return to_serialize
+        except AttributeError:
+            return super().default(o)
---
+++ tests/serializers/test_client.py
@@ -0,0 +1,31 @@
+"""Test module for client serializer"""
+import json
+import uuid
+
+from src.serializers.client import ClientJsonEncoder
+
+from src.domain.client import Client
+
+
+def test_serialize_domain_client():
+    """Test client model serialization"""
+    code = uuid.uuid4()
+    client = Client(
+        code,
+        razao_social="My company",
+        cnpj="00.000.000/0000-00",
+        email="mycompany@email.com",
+    )
+
+    excepted_json = f"""
+        {{
+            "code": "{code}",
+            "razao_social": "My company",
+            "cnpj": "00.000.000/0000-00",
+            "email": "mycompany@email.com"
+        }}
+        """
+
+    json_client = json.dumps(client, cls=ClientJsonEncoder)
+
+    assert json.loads(json_client) == json.loads(excepted_json)
