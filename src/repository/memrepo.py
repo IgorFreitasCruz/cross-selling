4d0b175eae6af6a99d6b36b0804934526b77df6c
Igor de Freitas Cruz <igor.freitas.cruz@icloud.com>
2023-11-14 15:11:10 -0300

    feat: implementing filters in MemRepo

--- src/repository/memrepo.py
+++ src/repository/memrepo.py
@@ -8,10 +8,21 @@ class MemRepo:
     def __init__(self, data) -> None:
         self.data = data
 
-    def list(self):
+    def list(self, filters=None):
         """List all clients
 
         Returns:
             List: List of client objectcs
         """
-        return [Client.from_dict(c) for c in self.data]
+        result = [Client.from_dict(c) for c in self.data]
+
+        if filters is None:
+            return result
+
+        if "code__eq" in filters:
+            result = [c for c in result if c.code == filters["code__eq"]]
+
+        if "ativo__eq" in filters:
+            result = [c for c in result if c.ativo is filters["ativo__eq"]]
+
+        return result
--- tests/repository/test_memrepo.py
+++ tests/repository/test_memrepo.py
@@ -1,9 +1,9 @@
 """Test module for client list use case"""
 # pylint: disable=w0621
 import pytest
-from src.repository.memrepo import MemRepo
 
 from src.domain.client import Client
+from src.repository.memrepo import MemRepo
 
 
 @pytest.fixture
@@ -18,25 +18,25 @@ def clients_dicts():
             "ativo": True,
         },
         {
-            "code": "f853578c-fc0f-4e65-81b8-566c5dffa35a",
+            "code": "fe2c3195-aeff-487a-a08f-e0bdc0ec6e9a",
             "razao_social": "My company 2",
             "cnpj": "00.000.000/0000-02",
             "email": "mycompany_2@email.com",
             "ativo": True,
         },
         {
-            "code": "f853578c-fc0f-4e65-81b8-566c5dffa35a",
+            "code": "913694c6-435a-4366-ba0d-da5334a611b2",
             "razao_social": "My company 3",
             "cnpj": "00.000.000/0000-03",
             "email": "mycompany_3@email.com",
-            "ativo": True,
+            "ativo": False,
         },
         {
-            "code": "f853578c-fc0f-4e65-81b8-566c5dffa35a",
+            "code": "eed76e77-55c1-41ce-985d-ca49bf6c0585",
             "razao_social": "My company 4",
             "cnpj": "00.000.000/0000-04",
             "email": "mycompany_4@email.com",
-            "ativo": True,
+            "ativo": False,
         },
     ]
 
@@ -52,3 +52,36 @@ def test_repository_list_without_parameters(clients_dicts):
     clients = [Client.from_dict(c) for c in clients_dicts]
 
     assert repo.list() == clients
+
+
+def test_repository_list_with_code_equal_filter(clients_dicts):
+    repo = MemRepo(clients_dicts)
+
+    clients = repo.list({"code__eq": "f853578c-fc0f-4e65-81b8-566c5dffa35a"})
+
+    assert len(clients) == 1
+    assert clients[0].code == "f853578c-fc0f-4e65-81b8-566c5dffa35a"
+
+
+def test_repository_list_with_ativo_equal_true_filter(clients_dicts):
+    repo = MemRepo(clients_dicts)
+
+    clients = repo.list({"ativo__eq": True})
+
+    assert len(clients) == 2
+    assert set([c.code for c in clients]) == {
+        "f853578c-fc0f-4e65-81b8-566c5dffa35a",
+        "fe2c3195-aeff-487a-a08f-e0bdc0ec6e9a",
+    }
+
+
+def test_repository_list_with_ativo_equal_false_filter(clients_dicts):
+    repo = MemRepo(clients_dicts)
+
+    clients = repo.list({"ativo__eq": False})
+
+    assert len(clients) == 2
+    assert set([c.code for c in clients]) == {
+        "913694c6-435a-4366-ba0d-da5334a611b2",
+        "eed76e77-55c1-41ce-985d-ca49bf6c0585",
+    }
